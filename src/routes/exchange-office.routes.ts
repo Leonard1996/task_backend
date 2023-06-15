ebc3f58215bc7da5c41767a8fea4ea2044386a35
Leonard <ldurmishi@airfind.com>
2023-06-16 01:30:06 +0200

    fix

--- src/routes/exchange-office.routes.ts
+++ src/routes/exchange-office.routes.ts
@@ -2,6 +2,6 @@ import * as express from "express";
 import { ExchangeOfficeController } from "../controllers/exchange-office.controller";
 export class ExchangeOfficeRouter {
   static configRoutes = (app: express.Application) => {
-    app.get("/", [ExchangeOfficeController.getHighestEarners]);
+    app.get("/exhange-offices", [ExchangeOfficeController.getHighestEarners]);
   };
 }
